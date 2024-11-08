654806aae0d9c41715864f42ee3917edfa862573
0xKate <94711905+0xKate@users.noreply.github.com.>
2024-11-07 22:51:47 -0800

    Add BelowZero Branch

--- ScubaTankConnector/Items/Equipment/OxygenLink.cs
+++ ScubaTankConnector/Items/Equipment/OxygenLink.cs
@@ -1,90 +1,16 @@
-﻿using Nautilus.Assets;
-using Nautilus.Assets.Gadgets;
-using Nautilus.Assets.PrefabTemplates;
-using Nautilus.Crafting;
-using Nautilus.Utility;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using static CraftData;
 
 namespace OxygenLink
 {
-    public static class OxygenLinkPrefab
-    {
-        public static PrefabInfo Info { get; } = PrefabInfo
-            .WithTechType("OxygenLink", "Oxygen Link", "Shares oxygen with all tanks in the inventory while equipped.")
-            .WithIcon(ImageUtils.LoadSpriteFromFile(System.IO.Path.Combine(Plugin.AssetFolder, "OxygenLink.png")))
-            .WithSizeInInventory(new Vector2int() { x = 1, y = 1 });
-        public static void Register()
-        {
-            var oxygenLinkPrefab = new CustomPrefab(Info);
-
-            var oxygenLinkObj = new CloneTemplate(Info, TechType.PlasteelTank);
-            oxygenLinkObj.ModifyPrefab += obj =>
-            {
-                GameObject.DestroyImmediate(obj.GetComponent<Oxygen>());
-                obj.AddComponent<OxygenLink>();
-            };
-
-            oxygenLinkPrefab.SetGameObject(oxygenLinkObj);
-            oxygenLinkPrefab
-                .SetRecipe(GetRecipe())
-                .WithFabricatorType(CraftTree.Type.Fabricator)
-                .WithStepsToFabricatorTab("Personal", "Equipment");
-            oxygenLinkPrefab.SetEquipment(EquipmentType.Tank);
-            oxygenLinkPrefab.SetPdaGroupCategory(TechGroup.Personal, TechCategory.Equipment);
-            oxygenLinkPrefab.Register();
-        }
-        public static RecipeData GetRecipe()
-        {
-            var numIngredients = (int)Settings.Current.RecipeDifficulty;
-            List<Ingredient> ingredients = Settings.Current.RecipeDifficulty switch
-            {
-                Difficulty.Easy => new List<Ingredient>()
-                    {
-                        new Ingredient(TechType.Silicone, 1),
-                        new Ingredient(TechType.Titanium, 3),
-                        new Ingredient(TechType.Lubricant, 2)
-                    },
-                Difficulty.Medium => new List<Ingredient>()
-                    {
-                        new Ingredient(TechType.Silicone, 2),
-                        new Ingredient(TechType.Titanium, 4),
-                        new Ingredient(TechType.Lubricant, 2),
-                        new Ingredient(TechType.Glass, 2),
-                        new Ingredient(TechType.ComputerChip, 1),
-                        new Ingredient(TechType.WiringKit, 1)
-                    },
-                Difficulty.Hard => new List<Ingredient>()
-                    {
-                        new Ingredient(TechType.Silicone, 4),
-                        new Ingredient(TechType.TitaniumIngot, 2),
-                        new Ingredient(TechType.Lubricant, 4),
-                        new Ingredient(TechType.EnameledGlass, 2),
-                        new Ingredient(TechType.AdvancedWiringKit, 2),
-                        new Ingredient(TechType.Polyaniline, 2),
-                        new Ingredient(TechType.Aerogel, 2),
-                    },
-                _ => throw new InvalidOperationException("Invalid difficulty level") { },
-            };
-
-            return new RecipeData()
-            {
-                craftAmount = 1,
-                Ingredients = ingredients
-            };
-        }
-    }
-
     public class OxygenLink : MonoBehaviour, IEquippable
     {
         private bool EventsRegistered = false;
         public List<Oxygen> LinkedSources = new List<Oxygen>();
         public void OnEquip(GameObject sender, string slot)
         {
+            FindAndLinkAllSources();
             RegisterEvents();
-            GetOxygenSources().ForEach(source => LinkOxygenSource(source));
             Plugin.Logger.LogDebug("OxygenLink Equipped!");
         }
         public void OnUnequip(GameObject sender, string slot)
@@ -112,34 +38,49 @@ namespace OxygenLink
         }
         public void LinkOxygenSource(Oxygen source)
         {
-            this.LinkedSources.Add(source);
-            Player.main.oxygenMgr.RegisterSource(source);
+            if (!this.LinkedSources.Contains(source))
+            {
+                this.LinkedSources.Add(source);
+                Player.main.oxygenMgr.RegisterSource(source);
+                Plugin.Logger.LogDebug($"Detected new oxygen source! [{source.name}]");
+            };
         }
         public void UnlinkOxygenSource(Oxygen source)
         {
-            this.LinkedSources.Remove(source);
-            Player.main.oxygenMgr.UnregisterSource(source);
+            if (this.LinkedSources.Contains(source))
+            {
+                this.LinkedSources.Remove(source);
+                Player.main.oxygenMgr.UnregisterSource(source);
+            };
+        }
+        public void FindAndLinkAllSources()
+        {
+            foreach (Oxygen source in GetOxygenSources())
+            {
+                LinkOxygenSource(source);
+            }
         }
         public void UnlinkAllSources()
         {
-            new List<Oxygen>(LinkedSources).ForEach(UnlinkOxygenSource);
+            List<Oxygen> sourcesCopy = new List<Oxygen>(this.LinkedSources);
+            foreach (Oxygen source in sourcesCopy)
+            {
+                UnlinkOxygenSource(source);
+            }
         }
+
         public void OnItemAdded(InventoryItem item)
         {
             if (item.item.gameObject.TryGetComponent<Oxygen>(out Oxygen source))
             {
-                if (!Player.main.oxygenMgr.sources.Contains(source))
-                {
-                    LinkOxygenSource(source);
-                    Plugin.Logger.LogDebug($"Detected new oxygen source! [{source.name}]");
-                };
+                LinkOxygenSource(source);
             }
         }
         public void OnItemRemoved(InventoryItem item)
         {
             if (item.item.gameObject.TryGetComponent<Oxygen>(out Oxygen source))
             {
-                if (Player.main.oxygenMgr.sources.Contains(source))
+                if (this.LinkedSources.Contains(source))
                 {
                     UnlinkOxygenSource(source);
                     Plugin.Logger.LogDebug($"Detected removed oxygen source! [{source.name}]");
---
+++ ScubaTankConnector/Items/Equipment/OxygenLinkPrefab.cs
@@ -0,0 +1,87 @@
+﻿using Nautilus.Assets;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Crafting;
+using Nautilus.Handlers;
+using Nautilus.Utility;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Nautilus.Assets.Gadgets;
+
+namespace OxygenLink
+{
+    public static class OxygenLinkPrefab
+    {
+        public static PrefabInfo Info { get; } = PrefabInfo
+            .WithTechType("OxygenLink", "Oxygen Link", "Shares oxygen with all tanks in the inventory while equipped.")
+            .WithIcon(ImageUtils.LoadSpriteFromFile(System.IO.Path.Combine(Plugin.AssetFolder, "OxygenLink.png")))
+            .WithSizeInInventory(new Vector2int() { x = 1, y = 1 });
+
+        public static void Register()
+        {
+            var oxygenLinkPrefab = new CustomPrefab(Info);
+
+            var oxygenLinkObj = new CloneTemplate(Info, TechType.Tank);
+            oxygenLinkObj.ModifyPrefab += obj =>
+            {
+                GameObject.DestroyImmediate(obj.GetComponent<Oxygen>());
+                obj.AddComponent<OxygenLink>();
+            };
+
+            oxygenLinkPrefab.SetGameObject(oxygenLinkObj);
+
+            CraftingGadget craftingGadget = oxygenLinkPrefab.SetRecipe(GetRecipe())
+            .WithFabricatorType(CraftTree.Type.Fabricator)
+            .WithStepsToFabricatorTab("Personal", "Equipment");
+
+            EquipmentGadget equipmentGadget = oxygenLinkPrefab.SetEquipment(EquipmentType.Tank);
+
+            oxygenLinkPrefab.SetPdaGroupCategory(TechGroup.Personal, TechCategory.Equipment);
+
+            KnownTechHandler.RemoveDefaultUnlock(Info.TechType);
+            KnownTechHandler.AddRequirementForUnlock(Info.TechType, TechType.Welder);
+
+            oxygenLinkPrefab.Register();
+            Plugin.Logger.LogInfo("Prefab OxygenLink is registered!");
+        }
+        public static RecipeData GetRecipe()
+        {
+            var numIngredients = (int)Settings.Current.RecipeDifficulty;
+            List<Ingredient> ingredients = Settings.Current.RecipeDifficulty switch
+            {
+                Difficulty.Easy => new List<Ingredient>()
+                    {
+                        new Ingredient(TechType.Silicone, 1),
+                        new Ingredient(TechType.Titanium, 3),
+                        new Ingredient(TechType.Lubricant, 2)
+                    },
+                Difficulty.Medium => new List<Ingredient>()
+                    {
+                        new Ingredient(TechType.Silicone, 2),
+                        new Ingredient(TechType.Titanium, 4),
+                        new Ingredient(TechType.Lubricant, 2),
+                        new Ingredient(TechType.Glass, 2),
+                        new Ingredient(TechType.ComputerChip, 1),
+                        new Ingredient(TechType.WiringKit, 1)
+                    },
+                Difficulty.Hard => new List<Ingredient>()
+                    {
+                        new Ingredient(TechType.Silicone, 4),
+                        new Ingredient(TechType.TitaniumIngot, 2),
+                        new Ingredient(TechType.Lubricant, 4),
+                        new Ingredient(TechType.EnameledGlass, 2),
+                        new Ingredient(TechType.AdvancedWiringKit, 2),
+                        new Ingredient(TechType.Polyaniline, 2),
+                        new Ingredient(TechType.Aerogel, 2),
+                    },
+                _ => throw new InvalidOperationException("Invalid difficulty level") { },
+            };
+
+            return new RecipeData()
+            {
+                craftAmount = 1,
+                Ingredients = ingredients
+            };
+        }
+    }
+}
